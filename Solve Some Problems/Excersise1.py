c38f292e7a14b5f5a0769105ef94bb3293091380
Nishant Parajuli <nishant.parajuli123@gmail.com>
2022-06-04 12:25:04 +0545

    brute force approach

---
+++ Solve Some Problems/Excersise1.py
@@ -0,0 +1,25 @@
+from array import array
+from tkinter.tix import Tree
+
+
+# Given 2 arrays, create a function that let's a user know(true/false) whether these two arrays contain any common items
+
+# array1 = ["a", "b", "c", "x"]
+# array2 = ["z", "y", "i"]
+# should return False
+
+# array1 = ["a", "b", "c", "x"]
+# array2 = ["z", "y", "x"]
+# Shoud return True
+
+
+# def findingCommon(array1, array2):  # first approach brute force
+#     a = False
+#     for item1 in array1:
+#         for item2 in array2:
+#             if item1 == item2:
+#                 a = True
+#     return a
+
+
+# print(findingCommon(array1, array2))
