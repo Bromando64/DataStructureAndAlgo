ee1b50ffc524e81e271e526ad875680499109fdb
Nishant Parajuli <nishant.parajuli123@gmail.com>
2022-06-05 17:11:06 +0545

    Built array using objects and classes

---
+++ DataStructure Array/BuildArray.py
@@ -0,0 +1,42 @@
+class MyArray:
+    def __init__(self):
+        self.length = 0
+        self.data = {}
+
+    def get(self, index):
+        return self.data[index]
+
+    def push(self, item):
+        self.data[self.length] = item
+        self.length += 1
+        return self.length
+
+    def pop(self):
+        lastItem = self.data[self.length - 1]
+        del self.data[self.length - 1]
+        self.length -= 1
+        return lastItem
+
+    def delete(self, index):
+        self.shiftItemsLeft(index)
+        del self.data[self.length - 1]
+        self.length -= 1
+
+    def replace(self, data,  index):
+        self.data[index] = data
+
+    def shiftItemsLeft(self, index):
+        for i in range(index, self.length - 1):
+            self.data[i] = self.data[i+1]
+
+
+newArray = MyArray()
+
+newArray.push("hi")
+newArray.push("you")
+newArray.push("!")
+newArray.push("hell")
+newArray.push("ball")
+newArray.push("doll")
+newArray.push("call")
+print(newArray.data)
