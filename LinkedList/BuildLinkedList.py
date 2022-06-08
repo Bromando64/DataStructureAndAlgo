f317608afb04de2d2426adf7554dc43366e77a08
Nishant Parajuli <nishant.parajuli123@gmail.com>
2022-06-08 16:54:14 +0545

    Single linked list

--- LinkedList/BuildLinkedList.py
+++ LinkedList/BuildLinkedList.py
@@ -37,16 +37,31 @@ class LinkedList:
         self.length += 1
 
     def insert(self, index, value):
+        # check the parameter
+        if index >= self.length:
+            return self.append(value)
         newNode = Node(value)
+        mover = self.traversal(index - 1)
+        temp = mover.next
+        mover.next = newNode
+        newNode.next = temp
+        self.length += 1
+
+    def delete(self, index):
+        if index >= self.length:
+            return "err"
+        mover = self.traversal(index - 1)
+        temp = mover.next
+        mover.next = temp.next
+        self.length -= 1
+
+    def traversal(self, index):
         mover = self.head
-        n = 1
+        n = 0
         while n < index:
             mover = mover.next
             n += 1
-        self.temp = mover.next
-        mover.next = newNode
-        newNode.next = self.temp
-        self.length += 1
+        return mover
 
     def printer(self):
         temp = self.head
@@ -63,4 +78,5 @@ myLinkedList.append(25)
 myLinkedList.append(16)
 myLinkedList.prepend(12)
 myLinkedList.insert(2, 99)
+myLinkedList.delete(3)
 myLinkedList.printer()
