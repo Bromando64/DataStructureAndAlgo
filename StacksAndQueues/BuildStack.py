9fb547cf2d420f149da2a58561894c7b9abd849d
Nishant Parajuli <nishant.parajuli123@gmail.com>
2022-06-09 17:17:22 +0545

    BuildStack File is created with template

---
+++ StacksAndQueues/BuildStack.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, value) -> None:
+        self.value = value
+        self.next - None
+
+
+class Stack:
+    def __init__(self) -> None:
+        self.top = None
+        self.bottom = None
+        self.length = 0
+
+    def peek(self):
+        pass
+
+    def push(self, value):
+        pass
+
+    def pop(self):
+        pass
+
+    def isEmpty(self):
+        pass
+
+
+myStack = Stack()
