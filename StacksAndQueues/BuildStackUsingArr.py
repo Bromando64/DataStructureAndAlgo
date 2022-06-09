baebcf5c592b6484c33a1d9367e3d2ffd908f767
Nishant Parajuli <nishant.parajuli123@gmail.com>
2022-06-09 19:56:04 +0545

    Stack is built using Array

---
+++ StacksAndQueues/BuildStackUsingArr.py
@@ -0,0 +1,20 @@
+class Stack:
+    def __init__(self) -> None:
+        self.arr = []
+
+    def peek(self):
+        return self.arr[len(self.arr)-1]
+
+    def push(self, value):
+        self.arr.append(value)
+
+    def pop(self):
+        self.arr.pop()
+
+
+myStack = Stack()
+myStack.push("Google")
+myStack.push("Amazon")
+myStack.push("Facebook")
+myStack.push("Discord")
+print(myStack.peek())
