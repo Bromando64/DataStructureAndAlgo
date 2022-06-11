66222dcda503f63d17c63abc9735d6e7546b5fe7
Nishant Parajuli <nishant.parajuli123@gmail.com>
2022-06-11 23:34:49 +0545

    queues using stack file was created

---
+++ StacksAndQueues/QueueBuiltUsingStack.py
@@ -0,0 +1,23 @@
+class MyQueue:
+
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+
+    def push(self, x: int) -> None:
+        pass
+
+    def pop(self) -> int:
+        pass
+
+    def peek(self) -> int:
+        pass
+
+    def empty(self) -> bool:
+        pass
+        # Your MyQueue object will be instantiated and called as such:
+        # obj = MyQueue()
+        # obj.push(x)
+        # param_2 = obj.pop()
+        # param_3 = obj.peek()
+        # param_4 = obj.empty()
