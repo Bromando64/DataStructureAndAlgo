7ebb4d44d4c91724d8000edc8e8c48e7abece06a
Nishant Parajuli <nishant.parajuli123@gmail.com>
2022-06-12 10:49:30 +0545

    Queue was built using stacks

--- StacksAndQueues/QueueBuiltUsingStack.py
+++ StacksAndQueues/QueueBuiltUsingStack.py
@@ -1,3 +1,29 @@
+# O(n) time complexity for push
+
+# class MyQueue:
+
+#     def __init__(self):
+#         self.s1 = []
+#         self.s2 = []
+
+#     def push(self, x: int) -> None:
+#         while self.s1:
+#             self.s2.append(self.s1.pop())
+#         self.s1.append(x)
+#         while self.s2:
+#             self.s1.append(self.s2.pop())
+
+#     def pop(self) -> int:
+#         return self.s1.pop()
+
+#     def peek(self) -> int:
+#         return self.s1[-1]
+
+#     def empty(self) -> bool:
+#         return not self.s1
+
+
+# O(1) time complexity for pop in average
 class MyQueue:
 
     def __init__(self):
@@ -5,19 +31,17 @@ class MyQueue:
         self.s2 = []
 
     def push(self, x: int) -> None:
-        pass
+        self.s1.append(x)
 
     def pop(self) -> int:
-        pass
+        self.peek()
+        return self.s2.pop()
 
     def peek(self) -> int:
-        pass
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2[-1]
 
     def empty(self) -> bool:
-        pass
-        # Your MyQueue object will be instantiated and called as such:
-        # obj = MyQueue()
-        # obj.push(x)
-        # param_2 = obj.pop()
-        # param_3 = obj.peek()
-        # param_4 = obj.empty()
+        return not self.s1 and not self.s2
