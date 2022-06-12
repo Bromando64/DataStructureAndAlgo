a21367ad65327815c7d2e52a4f85b977986ebda3
Nishant Parajuli <nishant.parajuli123@gmail.com>
2022-06-12 20:55:25 +0545

    created binary tree implementaion file

---
+++ BinaryTrees/BinaryTree.py
@@ -0,0 +1,21 @@
+class BinaryTreeNode:
+
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinarySearchTree:
+
+    def __init__(self) -> None:
+        self.root = None
+
+    def insert(value):
+        pass
+
+    def lookup(value):
+        pass
+
+    def remove(value):
+        pass
