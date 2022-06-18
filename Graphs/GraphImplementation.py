285e51611e374a64b0abffeb5d477245dfe8a8f2
Nishant Parajuli <nishant.parajuli123@gmail.com>
2022-06-18 15:23:39 +0545

    Graph was created using dictionaries and lists

--- Graphs/GraphImplementation.py
+++ Graphs/GraphImplementation.py
@@ -4,19 +4,15 @@ class Graph:
         self.adjacentList = {}
 
     def addVertex(self, node):
-        pass
+        self.adjacentList[node] = []
+        self.numberOfNodes += 1
 
     def addEdge(self, node1, node2):
-        pass
+        self.adjacentList[node1].append(node2)
+        self.adjacentList[node2].append(node1)
 
     def showConnections(self):
-        allNodes = self.adjacentList.keys
-        for node in allNodes:
-            nodeConnections = self.adjacentList[node]
-            connections = ""
-            for vertex in nodeConnections:
-                connections += vertex + " "
-            print(f"{node} --> {connections}")
+        print(self.adjacentList)
 
 
 myGraph = Graph()
